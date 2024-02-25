1afd63f65915c30ef73ff3f818d5bf6ca7d11e22
younes-karir <youneskarir@outlook.com>
2024-02-25 14:12:49 +0100

    initial commit

---
+++ src/test/java/com/youneskarir/springbootjpaonetomanycrud/SpringBootJpaOneToManyCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youneskarir.springbootjpaonetomanycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaOneToManyCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
