308c9f3e3a248d4960551bec41051f9965ace6d4
younes-karir <youneskarir@outlook.com>
2024-02-27 15:45:05 +0100

    custom responses

---
+++ src/main/java/com/youneskarir/springbootjpaonetomanycrud/response/ResponseHandler.java
@@ -0,0 +1,23 @@
+package com.youneskarir.springbootjpaonetomanycrud.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHandler {
+
+    public static ResponseEntity<Object> responseBuilder(
+            String message,
+            HttpStatus httpStatus,
+            Object responseObject
+    )
+    {
+        Map<String,Object> responses =  new HashMap<>();
+        responses.put("message",message);
+        responses.put("status" , httpStatus);
+        responses.put("data",responseObject);
+        return new ResponseEntity<>(responses,httpStatus);
+    }
+}
